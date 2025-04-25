bc59fb327f373cb78938966f7b97c82a46baec17
Gustavo Santos <gurezendes@gmail.com>
2025-04-25 12:26:06 -0400

    Add: added fallback model code

---
+++ chapter1/fallback.py
@@ -0,0 +1,26 @@
+# Imports
+import os
+from pydantic_ai import Agent
+from pydantic_ai.models.gemini import GeminiModel
+from pydantic_ai.models.fallback import FallbackModel
+from pydantic_ai.models.openai import OpenAIModel
+
+# Instantiate the models
+openai_model = OpenAIModel('gpt-4o')
+google_model = GeminiModel('gemini-1.5-flash')
+
+# Create the Fallback model
+fallback_model = FallbackModel(openai_model, google_model)
+
+# Run the agent
+agent = Agent(fallback_model,
+              retries=2,
+              api_base=os.environ.get("OPENAI_API_KEY"),
+              api_key=os.environ.get("GEMINI_API_KEY"),
+              system_prompt='Be concise, use a single sentence.')
+
+# Response
+response = agent.run_sync('What is the most populated city in the world?')
+print(response.output)
+
+print(response.all_messages())
