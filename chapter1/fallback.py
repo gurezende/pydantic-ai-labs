a1662edf3f5fcc80cec3f3d5d5b1d7acdb04b032
Gustavo Santos <gurezendes@gmail.com>
2025-04-25 12:50:20 -0400

    Upd: Fallback code updated

--- chapter1/fallback.py
+++ chapter1/fallback.py
@@ -4,6 +4,7 @@ from pydantic_ai import Agent
 from pydantic_ai.models.gemini import GeminiModel
 from pydantic_ai.models.fallback import FallbackModel
 from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.exceptions import ModelHTTPError
 
 # Instantiate the models
 openai_model = OpenAIModel('gpt-4o')
@@ -16,11 +17,16 @@ fallback_model = FallbackModel(openai_model, google_model)
 agent = Agent(fallback_model,
               retries=2,
               api_base=os.environ.get("OPENAI_API_KEY"),
-              api_key=os.environ.get("GEMINI_API_KEY"),
+            #   api_key=os.environ.get("GEMINI_API_KEY"),
               system_prompt='Be concise, use a single sentence.')
 
 # Response
-response = agent.run_sync('What is the most populated city in the world?')
-print(response.output)
+try:
+    response = agent.run_sync('What is the most populated city in the world?')
+    print(response.output)
+
+except ModelHTTPError as errors:
+    for error in errors:
+        print(error)
 
 print(response.all_messages())
